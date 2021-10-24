4292fba31e4a4647a803ee2fafd6b5c56c7bfc73
lu <xxx@xx.com>
2021-10-24 11:54:02 +0800

    first commit

---
+++ src/main/java/com/bjpowernode/App.java
@@ -0,0 +1,15 @@
+package com.bjpowernode;
+
+import javax.crypto.spec.PSource;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("hello");
+    }
+}
