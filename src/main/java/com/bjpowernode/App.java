86bbc4e09e2b7e60a5bf9891ad98a44024e3f50b
lu <xxx@xx.com>
2021-10-24 12:22:40 +0800

    second commit

--- src/main/java/com/bjpowernode/App.java
+++ src/main/java/com/bjpowernode/App.java
@@ -13,5 +13,6 @@ public class App
         System.out.println("hello");
         System.out.println("hello2");
         System.out.println("hello3");
+        System.out.println("hello4");
     }
 }
